46f42fca1eeeb3ed7ac8858ad4a5789908d0de3c
dev-Pedrod <pedroolivxira@gmail.com>
2022-02-26 11:20:34 -0300

    Project created

---
+++ src/main/java/com/devpedrod/TestsJunit/TestsJunitApplication.java
@@ -0,0 +1,13 @@
+package com.devpedrod.TestsJunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpedrod/TestsJunit/TestsJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpedrod.TestsJunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
