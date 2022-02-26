daeab6d99132d9ea3db621e95fdc174960d1cd3c
dev-Pedrod <pedroolivxira@gmail.com>
2022-02-26 12:41:06 -0300

    Test profile config

--- src/main/java/com/devpedrod/TestsJunit/controller/UserController.java
+++ src/main/java/com/devpedrod/TestsJunit/controller/UserController.java
@@ -2,9 +2,7 @@ package com.devpedrod.TestsJunit.controller;
 
 import com.devpedrod.TestsJunit.domain.User;
 import com.devpedrod.TestsJunit.services.IUserService;
-import com.devpedrod.TestsJunit.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
