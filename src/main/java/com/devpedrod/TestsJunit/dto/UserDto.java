29b1d5e9b284e24ae3f9f6a5c0f0b5321ab2884a
dev-Pedrod <pedroolivxira@gmail.com>
2022-02-28 11:40:54 -0300

    feat(User): User DTO

---
+++ src/main/java/com/devpedrod/TestsJunit/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.devpedrod.TestsJunit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private Long id;
+    private String name;
+    @Email
+    private String email;
+    private String password;
+}
